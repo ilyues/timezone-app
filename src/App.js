b7ce9985dafc16eb42763a211a4ca8340633ae85
Isabel Li <ilyues@berkeley.edu>
2021-02-14 12:58:55 +1300

    buttons (non functional)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
 import TeamProfile from "./TeamProfile";
+import edit from "./assets/edit.png";
+import clock from "./assets/clock.png";
 
 function App() {
   return (
     <div className="App">
-      <button>Calculate Time Difference</button> <button>Edit</button>
+      <div className="controls">
+        <button className="calculate">
+          <img src={clock} className="clock-png" alt="clock-icon" />
+          Calculate Time Difference
+        </button>{" "}
+        <input type="image" src={edit} className="edit-button" alt="edit" />
+      </div>
+
       <TeamProfile />
       <TeamProfile />
       <TeamProfile />
